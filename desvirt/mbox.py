f42d25d5eec6a1132e810f042ea5e776c77c65e8
Chamaeleon <y0067315@tu-bs.de>
2020-05-20 12:25:37 +0200

    added more function headers

--- desvirt/mbox.py
+++ desvirt/mbox.py
@@ -1,10 +1,6 @@
 import csv
-
-
-def get_loss(temp):
-    # TODO implement LUT
-    loss = temp
-    return loss
+import math
+from desvirt import vif
 
 
 def parse_temperatures(temperature_file):
@@ -13,7 +9,41 @@ def parse_temperatures(temperature_file):
         return list(temperature_lines)
 
 
-class MiddleBox():
-    in_tap = None
-    out_tap = None
+class MiddleBox:
+    in_if = None
+    out_if = None
+    from_if = None
+    to_if = None
     temperature_file = None
+    distance = None  # in meters
+    noise_floor = None
+    sensitivity = None
+    tx_power = None
+    temp_offset_lut = None
+    frequency = 2440  # in megahertz
+    fspl = 20 * math.log10(distance) + 20 * math.log10(frequency) - 27.55
+
+    def __init__(self):
+        self.in_if = vif.VirtualInterface.create()
+        self.out_if = vif.VirtualInterface.create()
+
+    def start(self):
+        temperatures = parse_temperatures(self.temperature_file)
+        # TODO start box process thread
+        pass
+
+    def box(self):
+        # TODO alter passing packages
+        pass
+
+    def calculate_rx_power(self):
+        pass
+
+    def get_temp_signal_offset(self, temp):
+        # TODO implement LUT
+        loss = temp
+        return loss
+
+    def calculate_ber(self):
+        #TODO implement ber
+        pass
