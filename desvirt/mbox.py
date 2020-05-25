a2c77232daa29da7159151453d26a801c4e21b89
Chamaeleon <y0067315@tu-bs.de>
2020-05-25 22:26:14 +0200

    added type hints to mbox

--- desvirt/mbox.py
+++ desvirt/mbox.py
@@ -1,7 +1,8 @@
 import csv
 import math
 import threading
-from desvirt import vif
+from typing import Optional
+from desvirt.vif import VirtualInterface
 
 
 def parse_temperatures(temperature_file):
@@ -22,7 +23,8 @@ class MiddleBox:
     temperature_file = None
     temp_offset_lut = None
 
-    def __init__(self, distance, noise_floor, sensitivity_offset, tx_power, frequency=2440, temperature_file=None):
+    def __init__(self, from_if: VirtualInterface, to_if: VirtualInterface, distance: float, noise_floor: float, sensitivity_offset: float, tx_power: float, frequency: float = 2440,
+                 temperature_file: str = None):
         self.thread = threading.Thread(target=self.box())
         self.in_if = vif.VirtualInterface.create()
         self.out_if = vif.VirtualInterface.create()
@@ -50,18 +52,18 @@ class MiddleBox:
         # TODO alter passing packages
         pass
 
-    def calculate_rx_power(self):
+    def calculate_rx_power(self) -> Optional[float]:
         # TODO add temp offset
         if (self.tx_power - self.fspl) > (self.noise_floor + self.sensitivity_offset):
             return self.tx_power - self.fspl
         else:
-            return False
+            return None
 
-    def get_temp_signal_offset(self, temp):
+    def get_temp_signal_offset(self, temp: float):
         # TODO implement LUT
         loss = temp
         return loss
 
-    def calculate_ber(self):
-        snr = self.calculate_rx_power()/self.noise_floor
+    def calculate_ber(self) -> float:
+        snr = self.calculate_rx_power() / self.noise_floor
         return min(8 * math.exp(-0.6 * (snr + 0.5)), 1.0)
